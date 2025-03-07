76ead14b8e46a346905a5c0a469c89217af8c26a
ebrahim <ebrahimasil@gmail.com>
2025-03-07 07:14:13 +0430

    Set up initial project structure

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,12 @@
-import React from 'react'
+import { BrowserRouter as Router } from "react-router-dom"
+import Navbar from "./components/Navbar"
 
 const App = () => {
   return (
     <div>
-      
+      <Router>
+        <Navbar />
+      </Router>
     </div>
   )
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,11 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        brand: "#a3153b",
+      }
+    },
   },
   plugins: [],
 }
