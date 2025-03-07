1ac1b2692ffe456570fa44d3159ab627449e63b1
ebrahim <ebrahimasil@gmail.com>
2025-03-07 07:01:36 +0430

    Initial commit

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,16 +1,9 @@
-import React from "react";
+import React from 'react'
 
 const Button = () => {
   return (
-    <button
-      className="lg:block text-base bg-[#c9184a] px-3 py-1 rounded text-white 
-        transition-all duration-300 hover:bg-[#a3153b] active:scale-95 focus:outline-none focus:ring-2 focus:ring-[#a3153b]"
-      
-      aria-label="Download Resume"
-    >
-      Resume
-    </button>
-  );
-};
+    <div>Button</div>
+  )
+}
 
-export default Button;
+export default Button
